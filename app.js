792a5acc91c4f65f78222b55e173a07cfc95b86c
David Napadensky <davidnapadensky@gmail.com>
2021-07-15 20:09:17 -0300

    tarea 2

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+// Requiere
+const fs = require('fs');
+const path = require('path');
+
+// Definir ruta del archivo
+const ruta = path.join(__dirname, 'tabla-5.txt')
+
+// crear variable para acumular tabla de 5
+let message = '';
+
+// for para iterar y crear tabla
 for (let index = 0; index < 10; index++) {
-  console.log(`5 X ${index + 1} = ${5 * (index+1)}`);
+  message = `${message}5 X ${index + 1} = ${5 * (index + 1)} \n`
 }
+
+// usar fs para guardar la tabla y terminar el programa
+fs.writeFile(ruta, message, function(err) {
+  if(err) throw err;
+  console.log('Archivo creado');
+});
