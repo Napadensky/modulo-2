4dab7e4777751b5d681a89942f27c953c2ab23e8
David Napadensky <davidnapadensky@gmail.com>
2021-07-19 20:39:06 -0300

    agregar pgjson y libreria de colores

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // requiriendo modulo
 const createTable = require('./createTable');
+const colors = require('colors');
 
 let message = ''; // crear variable para acumular tabla
 const [_,base] = process.argv[2].split('=') // obtener base
@@ -9,8 +10,11 @@ if(!Number(base)) throw new Error('base no es un numero') // validar que sea un 
 for (let index = 0; index < 10; index++) {
   message += `${base} X ${index + 1} = ${base * (index + 1)} \n`
 }
-console.log(message); // imprimir tabla por consola
+console.log('\n====================='.bgBlue);
+console.log(`Esta es la tabla del ${base}`.rainbow);
+console.log('====================='.bgBlue);
+console.log(message.rainbow); // imprimir tabla por consola
 
 createTable(message,base)
-  .then((succes) => console.log(succes))
-  .catch((error) => console.log(error))
+  .then((succes) => console.log(succes.green))
+  .catch((error) => console.log(error.red))
