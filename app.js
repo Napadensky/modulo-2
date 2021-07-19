baee74a8eab3a21f4fca6e02c4303351beec1d3d
David Napadensky <davidnapadensky@gmail.com>
2021-07-19 20:51:46 -0300

    agregando yarns

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // requiriendo modulo
 const createTable = require('./createTable');
 const colors = require('colors');
+const yargs = require('yargs')
+
 
 let message = ''; // crear variable para acumular tabla
-const [_,base] = process.argv[2].split('=') // obtener base
-if(!Number(base)) throw new Error('base no es un numero') // validar que sea un nuemero
+const {base} = yargs.argv // obtener base POR YARGS
+if (!Number(base)) throw new Error('base no es un numero') // validar que sea un nuemero
 
 // for para iterar y crear tabla
 for (let index = 0; index < 10; index++) {
@@ -15,6 +17,6 @@ console.log(`Esta es la tabla del ${base}`.rainbow);
 console.log('====================='.bgBlue);
 console.log(message.rainbow); // imprimir tabla por consola
 
-createTable(message,base)
+createTable(message, base)
   .then((succes) => console.log(succes.green))
   .catch((error) => console.log(error.red))
