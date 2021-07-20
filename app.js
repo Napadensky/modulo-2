798b059ab3653a5130bd0d0bdcd8b489187673ee
David Napadensky <davidnapadensky@gmail.com>
2021-07-20 16:39:13 -0300

    agregando configuraciones de yargs

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 // requiriendo modulo
 const createTable = require('./createTable');
 const colors = require('colors');
-const yargs = require('yargs')
-
-yargs.demand('base')
+const yargs = require('yargs').options('b',{
+  alias: 'base',
+  type: 'number',
+  demandOption: true
+}).argv
 
 let message = ''; // crear variable para acumular tabla
-const {base} = yargs.argv // obtener base POR YARGS
-if (!Number(base)) throw new Error('base no es un numero') // validar que sea un nuemero
+const {b} = yargs // obtener base POR YARGS
+if (!Number(b)) throw new Error('base no es un numero') // validar que sea un nuemero
 
 // for para iterar y crear tabla
 for (let index = 0; index < 10; index++) {
-  message += `${base} X ${index + 1} = ${base * (index + 1)} \n`
+  message += `${b} X ${index + 1} = ${b * (index + 1)} \n`
 }
 console.log('\n====================='.bgBlue);
-console.log(`Esta es la tabla del ${base}`.rainbow);
+console.log(`Esta es la tabla del ${b}`.rainbow);
 console.log('====================='.bgBlue);
 console.log(message.rainbow); // imprimir tabla por consola
 
-createTable(message, base)
+createTable(message, b)
   .then((succes) => console.log(succes.green))
   .catch((error) => console.log(error.red))
